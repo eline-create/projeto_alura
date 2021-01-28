1be276a5e4f3c1257580fb482e39969ea0eccddf
eline-create <eline.geo@gmail.com>
2021-01-27 23:38:27 -0300

    upload files

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,9 +1,21 @@
-const fs = require('fs')
+const fs = require("fs");
+const path = require("path");
 
-fs.readFile('./assets/salsicha.jpg', (erro, buffer) => { 
-    console.log('imagem foi bufferizada')
- 
-    fs.writeFile('./assets/salsicha2.jpg', buffer, (erro) => { 
-        console.log('imagem foi escrita')
-    })
-})
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+  const tiposValidos = ["jpg", "png", "jpeg"];
+  const tipo = path.extname(caminho);
+  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1;
+  const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`;
+
+  if (tipoEhValido) {
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`;
+
+    fs.createReadStream(caminho)
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on("finish", () => callbackImagemCriada(false, novoCaminho));
+  } else {
+    const erro = "Tipo é inválido";
+    console.log("Erro! Tipo inválido");
+    callbackImagemCriada(erro);
+  }
+};
