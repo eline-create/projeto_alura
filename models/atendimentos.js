0c7dffed8d4eb5c08f6aef02f69c24bb06a9ef08
eline-create <eline.geo@gmail.com>
2021-01-29 00:59:34 -0300

    conexao entre duas APIS

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,4 +1,5 @@
 const moment = require("moment");
+const axios = require("axios");
 const conexao = require("../infraestrutura/conexao");
 
 class Atendimento {
@@ -53,15 +54,20 @@ class Atendimento {
       }
     });
   }
-
+//Para funcionar, é preciso que dois terminais estejam abertos executando as portas das duas apis
   buscaPorId(id, res) {
     const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
 
-    conexao.query(sql, (erro, resultados) => {
+    conexao.query(sql, async (erro, resultados) => {
       const atendimento = resultados[0];
+      const cpf = atendimento.cliente;
       if (erro) {
         res.status(400).json(erro);
       } else {
+        const { data } = await axios.get(`http://localhost:8082/${cpf}`)
+
+        atendimento.cliente = data
+
         res.status(200).json(atendimento);
       }
     });
