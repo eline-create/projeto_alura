b7d9749c8ca674418b2845b693ebd1a16f72af39
eline-create <eline.geo@gmail.com>
2021-02-01 23:06:37 -0300

    Refatorando o post e o get

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -4,52 +4,59 @@ const conexao = require("../infraestrutura/database/conexao");
 const repositorio = require("../repositorios/atendimento");
 
 class Atendimento {
+  constructor() {
+    this.dataEhValida = ({ data, dataCriacao }) =>
+      moment(data).isSameOrAfter(dataCriacao);
+    this.clienteEhValido = (tamanho) => tamanho >= 5;
+    this.valida = (parametros) =>
+      this.validacoes.filter((campo) => {
+        const { nome } = campo;
+        const parametro = parametros[nome];
+
+        return !campo.valido(parametro);
+      });
+    this.validacoes = [
+      {
+        nome: "data",
+        valido: this.dataEhValida,
+        mensagem: "Data deve ser maior ou igual a data atual",
+      },
+      {
+        nome: "cliente",
+        valido: this.clienteEhValido,
+        mensagem: "Cliente deve ter pelo menos cinco caracteres",
+      },
+    ];
+  }
+
   adiciona(atendimento) {
     const dataCriacao = moment().format("YYYY-MM-DD HH:MM:SS");
     const data = moment(atendimento.data, "DD/MM/YYYY").format(
       "YYYY-MM-DD HH:MM:SS"
     );
-    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
-    const clienteEhValido = atendimento.cliente.length >= 5;
 
-    const validacoes = [
-      {
-        nome: "data",
-        valido: dataEhValida,
-        mensagem: "Data deve ser maior ou igual a data atual",
-      },
-      {
-        nome: "cliente",
-        valido: clienteEhValido,
-        mensagem: "Cliente deve ter pelo menos cinco caracteres",
-      },
-    ];
-
-    const erros = validacoes.filter((campo) => !campo.valido);
+    const parametros = {
+      data: { data, dataCriacao },
+      cliente: { tamanho: atendimento.cliente.length },
+    };
+    const erros = this.valida(parametros);
     const existemErros = erros.length;
+
     if (existemErros) {
-      return new Promise((resolve, reject) => reject(erros))
+      return new Promise((resolve, reject) => reject(erros));
     } else {
       const atendimentoDatado = { ...atendimento, dataCriacao, data };
 
-      return repositorio.adiciona(atendimentoDatado)
-        .then((resultados) => {
-          const id = resultados.insertId;
-          return { ...atendimento, id };
+      return repositorio.adiciona(atendimentoDatado).then((resultados) => {
+        const id = resultados.insertId;
+        return { ...atendimento, id };
       });
     }
   }
 
-  lista(res) {
-    const sql = "SELECT * FROM Atendimentos";
-
-    conexao.query(sql, (erro, resultados) => {
-      if (erro) {
-        res.status(400).json(erro);
-      } else {
-        res.status(200).json(resultados);
-      }
-    });
+  lista() {
+    return repositorio.lista()
+    
   }
   //Para funcionar, é preciso que dois terminais estejam abertos executando as portas das duas apis
   buscaPorId(id, res) {
